84c66c958a0e33fad8bb83dd3d196f9d59b05d5e
david1597-embedded <gkwkqnxk159@gmail.com>
2025-07-23 19:48:06 +0900

    mudulize ready

---
+++ camera/installrequirements.py
@@ -0,0 +1,15 @@
+import requests
+from pathlib import Path
+
+
+if not Path("notebook_utils.py").exists():
+    r = requests.get(
+        url="https://raw.githubusercontent.com/openvinotoolkit/openvino_notebooks/latest/utils/notebook_utils.py",
+    )
+
+    open("notebook_utils.py", "w").write(r.text)
+
+# Read more about telemetry collection at https://github.com/openvinotoolkit/openvino_notebooks?tab=readme-ov-file#-telemetry
+from midas_depth.notebook_utils import collect_telemetry
+
+collect_telemetry("vision-monodepth.ipynb")
