00ceea21df3d7c46d908eb37b9962c6ffd841148
david1597-embedded <gkwkqnxk159@gmail.com>
2025-07-23 19:34:28 +0900

    Fix: updated motor control logic

---
+++ motors/control.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+import numpy as np
+
+class MOTORCONTROLLER:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        #변수 다 초기화
+
+    def moveforward(self):
+        pass
+    def movebackward(self):
+        pass
+    def rotateright(self):
+        pass
+    def rotateleft(self):
+        pass
+    def measure_distnace(self, point_x, point_y):
+        pass
+
+    
+      
+
